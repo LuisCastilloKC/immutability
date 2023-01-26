bdad5a0b4623abd58e4bd8e8ce62afe28725a5df
Luis Castillo <mr.luiscastillo@gmail.com>
2023-01-25 22:03:15 -0500

    immutable and mutuable

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,18 @@
 fun main(args: Array<String>) {
+    println("Hello, World")
+    // No need to declare new to make an instance
+    val q = Question()
+   // q = Question()
 
+    q.Answer = "42"
+    // q.Question can't be re-assign because key word val
+
+    // val  --- are immutable
+    // var  --- are mutuable
+    q.Question = ""
+}
+
+class Question {
+    var Answer:String = ""
+    val Question: String = "What is the answer to life, he universe and everything?"
 }
